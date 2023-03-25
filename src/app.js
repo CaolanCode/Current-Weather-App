a5daebe387fe5afd9b16a8f1e41be5173d447c75
CaolanCode <cmacmahon3@gmail.com>
2023-03-25 11:23:02 +0000

    commit header design

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 import {displayWeatherData} from './call-api.js'
+import {header} from './ui.js'
+import './styles/style.css'
 
-let city = prompt("Enter city")
-city.charAt(0).toUpperCase()
+// google symbol link 
+const link = document.createElement('link')
+link.rel = 'stylesheet'
+link.href = "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+document.head.appendChild(link)
 
-displayWeatherData(city)
+/*
+const city = prompt("Enter city")
+const capCity = city.charAt(0).toUpperCase() + city.slice(1)
+displayWeatherData(capCity)
+*/
+
+// append UI
+document.body.appendChild(header())
+
