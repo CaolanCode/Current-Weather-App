cf97d98d04dae48cfcc73a10680ffff26e95d353
CaolanCode <cmacmahon3@gmail.com>
2023-03-25 15:54:01 +0000

    commit styling for todays weather

--- src/ui.js
+++ src/ui.js
@@ -24,66 +24,67 @@ export const displayDataToday = ({city, tempC, tempF, condName, condIcon, time, 
   container.classList.add('display-container')
   // city text
   const title = document.createElement('div')
-  title.classList.add('city-title')
+  title.classList.add('city')
   title.innerText = city
   // temperature
   const temp = document.createElement('div')
-  temp.classList.add('temp-today')
-  temp.innerText = tempC
+  temp.classList.add('temp')
+  temp.innerText = tempC + '°C'
   // temp conversion
   const conversion = document.createElement('div')
   conversion.classList.add('conversion-container')
+  conversion.innerText = 'Change: '
   const celcius = document.createElement('button')
-  celcius.classList.add('celcius-btn')
+  celcius.classList.add('temp-btn')
   celcius.innerText = '°C'
   const fahrenheit = document.createElement('button')
-  fahrenheit.classList.add('celcius-btn')
+  fahrenheit.classList.add('temp-btn')
   fahrenheit.innerText = '°F'
   conversion.appendChild(celcius)
   conversion.appendChild(fahrenheit)
   // condition
   const condition = document.createElement('div')
-  condition.classList.add('condition-container-today')
+  condition.classList.add('condition-container')
   const condText = document.createElement('div')
-  condText.classList.add('cond-text-today')
+  condText.classList.add('cond-text')
   condText.innerText = condName
   const condImage = document.createElement('img')
-  condImage.classList.add('cond-image-today')
-  condImage.innerHTML = condIcon
-  condition.appendChild(condText)
+  condImage.classList.add('cond-image')
+  condImage.src = condIcon
   condition.appendChild(condImage)
+  condition.appendChild(condText)
   // time
   const cityTime = document.createElement('div')
-  cityTime.classList.add('time-text-today')
+  cityTime.classList.add('time-text')
   cityTime.innerText = time
   // wind
   const wind = document.createElement('div')
-  wind.classList.add('div')
-  wind.innerText = windKPH
+  wind.classList.add('wind')
+  wind.innerText = 'Wind ' + windKPH + 'kph'
   // lat & lon
   const latLon = document.createElement('div')
   latLon.classList.add('div')
   const lat = document.createElement('div')
-  lat.classList.add('lat-city')
-  lat.innerText = Math.floor(latitude)
+  lat.classList.add('lat')
+  lat.innerText = 'Latitude: ' + Math.floor(latitude)
   const lon = document.createElement('div')
-  lon.classList.add('lon-city')
-  lon.innerText = Math.floor(longitude)
+  lon.classList.add('lon')
+  lon.innerText = 'Longitude: ' + Math.floor(longitude)
   latLon.appendChild(lat)
   latLon.appendChild(lon)
   // event listeners
   celcius.addEventListener('click', () => {
-    temp.innerText = tempC
-    wind.innerText = windKPH
+    temp.innerText = tempC + '°C'
+    wind.innerText = 'Wind ' + windKPH + 'kph'
   })
   fahrenheit.addEventListener('click', () => {
-    temp.innerText = tempF
-    wind.innerText = windMPH
+    temp.innerText = tempF + '°F'
+    wind.innerText = 'Wind ' + windMPH + 'mph'
   })
   // append
-  container.appendChild(title)
   container.appendChild(temp)
   container.appendChild(conversion)
+  container.appendChild(title)
   container.appendChild(condition)
   container.appendChild(cityTime)
   container.appendChild(wind)
